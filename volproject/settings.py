1eea97d62761cff7f4caf4e363159429714b9b95
racercode <convenient0610@gmail.com>
2022-07-10 20:10:59 +0800

    change debug mode

--- volproject/settings.py
+++ volproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'https://vocabulary-learn.herokuapp.com/',
